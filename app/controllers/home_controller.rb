bdc7c966fc1c3977077f4ac8a8ee698efb0d9555
laptop <you@example.com>
2014-09-08 16:24:52 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,45 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, only: [:book_class , :favorite]
   
   def welcome
-    if current_student.present?
-      @fav = Favorite.where('student_id = ?' , current_student.id)
-    else
-      @featured_class = Lesson.where(:featured => true)
-    end
+    # if current_student.present?
+    #   @fav = Favorite.where('student_id = ?' , current_student.id)
+    # else
+    #   @featured_class = Lesson.where(:featured => true)
+    # end
+    @tempp=Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
   end
   
   def class_search
+
     if params[:price_search].present? and params[:price_search] == "Low to High"
       @lessons=Lesson.order(:price)
       render @lessons, layout: false
     elsif params[:price_search].present? and params[:price_search] == "High to Low"
       @lessons = Lesson.order("price DESC").all
       render @lessons, layout: false
+    elsif params[:price_search].present? and params[:price_search] == "All Classes"
+      @lessons = Lesson.all
+      render @lessons, layout: false
     elsif params[:neighbourhood].present?
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "With in 1 Km"
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "With in 2 Km"
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "With in 3 Km"
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "With in 4 Km"
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "With in 5 Km"
+      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      render @lessons, layout: false
+    elsif params[:distance].present? and params[:distance] == "More than 5 Km"
+      binding.pry
       @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
       render @lessons, layout: false
     elsif params[:search].present?
