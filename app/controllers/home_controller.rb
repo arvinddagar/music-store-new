392265cb465565b5424c6e4cafdd8940d17d02de
laptop <you@example.com>
2014-09-08 17:08:32 +0530

    work on class distance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
   end
   
   def class_search
-    
+    address = current_student.address if current_student.present?
     if params[:price_search].present? and params[:price_search] == "Low to High"
       @lessons=Lesson.order(:price)
       render @lessons, layout: false
@@ -24,23 +24,22 @@ class HomeController < ApplicationController
       @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "With in 1 Km"
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.near(address, 1)
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "With in 2 Km"
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.near(address, 2)
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "With in 3 Km"
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.near(address, 3)
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "With in 4 Km"
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.near(address, 4)
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "With in 5 Km"
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.near(address, 5)
       render @lessons, layout: false
     elsif params[:distance].present? and params[:distance] == "More than 5 Km"
-      binding.pry
-      @lessons = Lesson.where(neighbourhood: params[:neighbourhood])
+      @lessons = Lesson.all
       render @lessons, layout: false
     elsif params[:search].present?
   	  @lessons=Lesson.search(params[:search])
