de6864b44bed2b7b79c95f3c55b0caa033339ca5
laptop <you@example.com>
2014-09-19 12:24:31 +0530

    changes made in controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   skip_before_filter :redirect_user_with_incomplete_registration_stu,
                      only: [:complete_registration_stu, :complete_stu]
     before_filter :authenticate_user!,
-                only: [:index, :update,:complete_registration_stu,:mylist,:rate]
+                only: [:index,:reservations, :update,:complete_registration_stu,:mylist,:rate]
   def index
     @upcoming_reservations= current_student.reservations.where("class_date > ?", Date.today)
   end
