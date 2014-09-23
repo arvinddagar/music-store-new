d377fb813f4df3467b3e84ab66f2b21958ff9f6d
laptop <you@example.com>
2014-09-23 18:00:53 +0530

    changes made

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            '99slips@gmail.com',
-      password:             '99slipsdummy',
+      user_name:            'tester.testing.again@gmail.com',
+      password:             'alms007alms',
       authentication:       'plain',
       enable_starttls_auto: true  }
     ActionMailer::Base.delivery_method = :smtp
