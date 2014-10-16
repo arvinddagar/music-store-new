219ad81f0fa2325744b795980c442a28eea4d593
laptop <you@example.com>
2014-10-16 15:08:43 +0530

    changes made

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -13,16 +13,29 @@ class Devise::SessionsController < DeviseController
   end
 
   # POST /resource/sign_in
+
   def create
-    @user = User.find_by "email = ? " ,params[:user][:email]
-    if @user and @user.confirmed_at and @user.valid_password?(params[:user][:password])
-      self.resource = warden.authenticate!(auth_options)
-      set_flash_message(:notice, :signed_in) if is_flashing_format?
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
-    end
+    string_url = request.original_url
+    if string_url.include? "admin"
+      if request.xhr?.nil?  || !warden.authenticate(auth_options).nil?
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+      end
+    else
+      @user = User.find_by "email = ? " ,params[:user][:email]
+      if @user and @user.confirmed_at and @user.valid_password?(params[:user][:password])
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+      end
+    end  
   end
+
 
   # DELETE /resource/sign_out
   def destroy
