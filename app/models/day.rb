fdef5909a8738860d6a4618a61440cecd6cf13c6
laptop <you@example.com>
2014-10-20 12:46:31 +0530

    Changes made

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,6 @@
 class Day < ActiveRecord::Base
   belongs_to :schedule
   has_many :timings, :dependent => :destroy
-  accepts_nested_attributes_for :timings,:allow_destroy => true
+  accepts_nested_attributes_for :timings,:allow_destroy => true, :reject_if => lambda { |a| a[:start_time  ].blank? } ,:allow_destroy => true
+
 end
